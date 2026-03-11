092c3516eb65a526188d425da7f06bd248deb7d4
shreyas <shreyaskalvankar@gmail.com>
2026-03-11 11:06:19 +0100

    update interim thesis presentation

--- interim_presentation/scenes/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib import import_module
-
-ThesisIntro = import_module(".01_thesis_intro", __name__).ThesisIntro
-SupervisedLearningFramework = import_module(
-    ".02_supervised_learning_framework", __name__
-).SupervisedLearningFramework
-
-__all__ = ["SupervisedLearningFramework", "ThesisIntro"]
